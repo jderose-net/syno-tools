0ac4595b45bd8736e5623d31b6ebda900c834a92
John DeRose <jderose@jderose.net>
2024-12-12 11:24:31 -0700

    added (some) error handling and an option to run without assigning the interface an ip address.

--- ovs-vlans.sh
+++ ovs-vlans.sh
@@ -1,12 +1,18 @@
 #!/bin/sh
 
 # the purpose of this script is to create a Vlan interface via an Open vSwitch
-# bridge, assign it an address, and test connectivity to the router.
+# bridge; optionally, assign it an address and test connectivity to the router.
 #
 # note: must be run as root (i.e., sudo); also, be sure to chmod +x the file.
 
-if [ "$#" -ne 5 ]; then
-    echo "usage: `basename $0` <PARENT> <BRIDGE> <VLANID> <CIDRIP> <ROUTER>"
+if [ "$#" -ne 3 ] && [ "$#" -ne 5 ]; then
+    echo "usage: $(basename $0) <PARENT> <BRIDGE> <VLANID> [<CIDRIP> <ROUTER>]"
+    echo "# <PARENT> is the name of the trunk interface (e.g., ovs_eth0)."
+    echo "# <BRIDGE> is the name of the bridge interface that will be created."
+    echo "# <VLANID> is the vlan that will be assigned to the bridge interface."
+    echo "# optional:"
+    echo "# <CIDRIP> is the address to assign to the bridge interface."
+    echo "# <ROUTER> is the address to ping to test the bridge interface."
     exit 1
 fi
 
@@ -14,15 +20,36 @@ fi
 #   sudo ./ovs-vlans.sh ovs_eth5 eth5-br11 11 10.0.11.99/24 10.0.11.1
 # 
 # program will create a bridge interface (eth5-br11) off parent (ovs_eth5),
-# assigned address (10.0.11.99) with broadcast (10.0.11.255; calculated).
+# assigned address (10.0.11.99) with broadcast (10.0.11.255; calculated);
 # it will then attempt to ping a gateway (10.0.11.1) to test the interface.
+#
+# note: if you do not pass a CIDRIP and ROUTER, you can specify these values
+# using the Synology Network control panel (which might be more intuitive for
+# some); however, do not select "Enable VLAN (802.1Q)", as the interface is
+# already bound to the vlan you specified when creating the interface and
+# will not pass traffic if "double-tagged". importantly, addresses set in the
+# UI are not automatically brought up on reboot (#bug).
+#
+# 2024-12-11: tested on DSM 7.2.2-72806 Update 2 (hw: RS1619xs+ w/10GbE)
 
-PARENT="$1"		# existing interface
+PARENT="$1"		# name of the trunk interface (e.g., eth0)
 BRIDGE="$2"		# vlan bridge interface (to be created)
-VLANID="$3"
-CIDRIP="$4"
-ROUTER="$5"
-BRDCST=$(ipcalc -b $CIDRIP | grep BROADCAST | cut -d '=' -f 2)
+VLANID="$3"		# vlan to be assigned to the bridge interface
+
+if [ "$#" -eq 5 ]; then
+    CIDRIP="$4"
+    ROUTER="$5"
+    BRDCST=$(ipcalc -b $CIDRIP | grep BROADCAST | cut -d '=' -f 2)
+fi
+
+error() {
+    echo "## $2 failed"
+    exit 2
+}
+
+trap 'error $LINENO $BASH_COMMAND' ERR
+
+# https://www.openvswitch.org/support/dist-docs/ovs-vsctl.8.html
 
 echo "## deleting bridge interface"
 ovs-vsctl --if-exists del-br $BRIDGE
@@ -30,21 +57,16 @@ ovs-vsctl --if-exists del-br $BRIDGE
 echo "## creating bridge interface"
 ovs-vsctl add-br $BRIDGE $PARENT $VLANID
 
-echo "## setting ip addr on bridge interface"
-ip addr flush dev $BRIDGE
-ip addr add $CIDRIP brd $BRDCST dev $BRIDGE
-
 echo "## bringing up interface"
-ip link set $BRIDGE up
+ip link set $BRIDGE up || error_exit
 
-echo "## pinging the router"
-ping -c 1 -w 1 -q $ROUTER
+if [ -n "$CIDRIP" ] && [ -n "$ROUTER" ]; then
+    echo "## setting ip addr on bridge interface"
+    ip addr flush dev $BRIDGE
+    ip addr add $CIDRIP brd $BRDCST dev $BRIDGE
 
-if [ $? -eq 0 ]; then
-    echo "## passed"
-	exit 0
-else
-    echo "## failed"
-	exit 1
+    echo "## pinging the router"
+    ping -c 1 -w 1 -q -I $BRIDGE $ROUTER
+    [ $? -eq 0 ] && echo "## passed"
 fi
 
